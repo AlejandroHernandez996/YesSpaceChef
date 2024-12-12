d0f5ccdf03ef8a0ec1d285402dff37c4045aa774
alejandrohernandez996 <alejandrohernandez996@gmail.com>
2024-12-12 10:20:02 -0600

    init

---
+++ Source/YesSpaceChef/YesSpaceChefGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "YesSpaceChefGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AYesSpaceChefGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AYesSpaceChefGameMode();
+};
+
+
+
